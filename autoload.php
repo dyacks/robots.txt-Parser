69c98fadc5def20088331c2db69daed5a9da97f0
Dyachuk Sergey <dyachukSerge@gmail.com>
2017-02-07 00:22:57 +0200

    autoload only namespace

--- autoload.php
+++ autoload.php
@@ -1,20 +1,20 @@
 <?php
 
 function __autoload($class){
+    /*
+           if (file_exists(__DIR__ . '/models/' . $class . '.php')) {
+                require __DIR__ . '/models/' . $class . '.php';
 
-        if (file_exists(__DIR__ . '/models/' . $class . '.php')) {
-            require __DIR__ . '/models/' . $class . '.php';
+            } elseif (file_exists(__DIR__ . '/views/' . $class . '.php')) {
+                require __DIR__ . '/views/' . $class . '.php';
 
-        } elseif (file_exists(__DIR__ . '/views/' . $class . '.php')) {
-            require __DIR__ . '/views/' . $class . '.php';
+            } elseif (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
+                require __DIR__ . '/controllers/' . $class . '.php';
 
-        } elseif (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
-            require __DIR__ . '/controllers/' . $class . '.php';
-
-        } elseif (file_exists(__DIR__ . '/classes/' . $class . '.php')) {
-            require __DIR__ . '/classes/' . $class . '.php';
-        } else {
-
+            } elseif (file_exists(__DIR__ . '/classes/' . $class . '.php')) {
+                require __DIR__ . '/classes/' . $class . '.php';
+            } else {
+*/
             $classParts = explode('\\', $class);
             $classParts[0] = __DIR__;
             $path = implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
@@ -24,9 +24,6 @@ function __autoload($class){
             } else {
                 throw new \RuntimeException("Class: " . $class . " - Not Found!");
             }
-        }
-
-
 
 }
 
